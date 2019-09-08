058c49691a2f6536acb7e91195b6de838ac02dfc
erictepper <eric.tepper@me.com>
2019-09-07 22:19:47 -0700

    Hopefully added basic functionality for a library search.

--- force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
+++ force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
@@ -1,3 +1,8 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 
-export default class LibraryCatalog extends LightningElement {}
+import getLibraryItems from '@salesforce/apex/catalogController.getLibraryItems';
+
+export default class LibraryCatalog extends LightningElement {
+    @wire(getLibraryItems, {})
+    libraryItems;
+}
