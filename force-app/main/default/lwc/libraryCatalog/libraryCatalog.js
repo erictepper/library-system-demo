cf73bec46829de16e89fe8e79f5143958ad7b233
erictepper <eric.tepper@me.com>
2019-09-08 00:29:04 -0700

    Added event trigger for updating name search field, changed wire to pass in search parameters and properly handle the data.

--- force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
+++ force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
@@ -1,8 +1,29 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 
 import getLibraryItems from '@salesforce/apex/catalogController.getLibraryItems';
 
 export default class LibraryCatalog extends LightningElement {
-    @wire(getLibraryItems)
-    libraryItems;
+    @track libraryItems;
+    @track nameSearch = "";
+    @track typeSearch = "";
+
+    @wire(getLibraryItems, { 
+        nameSearch: '$nameSearch',
+        typeSearch: '$typeSearch' 
+    })
+    wiredLibraryItems({ error, data }) {
+        if (data) {
+            this.libraryItems = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.contacts = undefined;
+        }
+    }
+
+    changeHandler(event) {
+        this.nameSearch = event.target.value;
+        refreshApex(this.wiredLibraryItems);
+    }
 }
