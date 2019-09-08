3798f9abe0f18204c5f300ab28755650f56369e7
erictepper <eric.tepper@me.com>
2019-09-07 22:59:08 -0700

    Removed unnecessary wire argument.

--- force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
+++ force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
@@ -3,6 +3,6 @@ import { LightningElement, wire } from 'lwc';
 import getLibraryItems from '@salesforce/apex/catalogController.getLibraryItems';
 
 export default class LibraryCatalog extends LightningElement {
-    @wire(getLibraryItems, {})
+    @wire(getLibraryItems)
     libraryItems;
 }
