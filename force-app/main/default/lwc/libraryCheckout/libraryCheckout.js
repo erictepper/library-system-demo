d11eb873d38c8cc7bb0069f808b3f4234bc00b8f
erictepper <eric.tepper@me.com>
2019-09-09 22:28:28 -0700

    Added variabled and method to wire the highest barcode from the Apex controller class.

--- force-app/main/default/lwc/libraryCheckout/libraryCheckout.js
+++ force-app/main/default/lwc/libraryCheckout/libraryCheckout.js
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 
-export default class LibraryCheckout extends LightningElement {}
+import getHighestAvailableBarcode from '@salesforce/apex/controller.getHighestAvailableBarcode';
+
+export default class LibraryCheckout extends LightningElement {
+    @track barcodeUpdate;
+    @track userSearch = "";
+    @track barcodeSearch = "";
+
+    @wire(getHighestAvailableBarcode)
+    wiredBarcode(barcodeUpdate) {
+        this.barcodeUpdate = barcodeUpdate;
+        const { error, data } = barcodeUpdate;
+        if (data) {
+            this.barcodeSearch = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.contacts = undefined;
+        }
+    }
+}
