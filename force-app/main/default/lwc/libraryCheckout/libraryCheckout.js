34a4aa25bf76464a95b132cfa610005aa78515f7
erictepper <eric.tepper@me.com>
2019-09-09 22:41:57 -0700

    Fixed code for barcode input and added method to wire a checkout from the library.

--- force-app/main/default/lwc/libraryCheckout/libraryCheckout.js
+++ force-app/main/default/lwc/libraryCheckout/libraryCheckout.js
@@ -2,9 +2,11 @@ import { LightningElement, track, wire } from 'lwc';
 import { refreshApex } from '@salesforce/apex';
 
 import getHighestAvailableBarcode from '@salesforce/apex/controller.getHighestAvailableBarcode';
+import checkout from '@salesforce/apex/controller.checkout';
 
 export default class LibraryCheckout extends LightningElement {
-    @track barcodeUpdate;
+    barcodeUpdate;
+    @track updateResult;
     @track userSearch = "";
     @track barcodeSearch = "";
 
@@ -21,11 +23,28 @@ export default class LibraryCheckout extends LightningElement {
         }
     }
 
+    @wire(checkout, { 
+        employeeId: '$userSearch',
+        barcode: '$barcodeSearch'
+    })
+    wiredCheckout(checkoutUpdate) {
+        this.checkoutUpdate = checkoutUpdate;
+        const { error, data } = checkoutUpdate;
+        if (data) {
+            this.updateResult = data;
+            this.error = undefined;
+        } else if (error) {
+            this.updateResult = undefined;
+            this.error = error;
+        }
+    }
+
     changeHandler(event) {
         var re = new RegExp('([A-Za-z]+)-?\\d*');
         var source = event.target.id.match(re);
         switch (source[1]) {
             case 'bar':
+                event.target.value = this.barcodeSearch;
                 break;
             default:
                 this.nameSearch = source[1];
