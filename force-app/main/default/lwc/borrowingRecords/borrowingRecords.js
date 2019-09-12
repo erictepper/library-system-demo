3cc21cb91341170a3b79c16d95759b61266820d2
erictepper <eric.tepper@me.com>
2019-09-12 16:34:01 -0700

    Added search input functionality.

--- force-app/main/default/lwc/borrowingRecords/borrowingRecords.js
+++ force-app/main/default/lwc/borrowingRecords/borrowingRecords.js
@@ -1,10 +1,24 @@
 import { LightningElement, track, wire } from 'lwc';
+import { refreshApex } from '@salesforce/apex';
 
 import getBorrowingUsers from '@salesforce/apex/controller.getBorrowingUsers';
 
 export default class BorrowingHistory extends LightningElement {
-    @track checkoutUpdate
+    // apex refresh fields
+    @track checkoutUpdate;
+    @track recordsUpdate;
+
+    // wire result lists
     @track borrowingUsers;
+    @track checkoutRecords;
+
+    // record search fields
+    @track usernameSearch = "";
+    @track barcodeSearch = "";
+    @track typeSearch = "";
+    @track itemNameSearch = "";
+    @track checkoutSearch;
+    @track returnSearch;
 
     @wire(getBorrowingUsers, {})
     wiredLibraryItems(checkoutUpdate) {
@@ -23,5 +37,24 @@ export default class BorrowingHistory extends LightningElement {
             this.error = error;
             this.borrowingUsers = undefined;
         }
+    }
+
+    get typeOptions() {
+        return [
+            { label: 'All', value: '' },
+            { label: 'AV', value: 'AV Equipment' },
+            { label: 'Book', value: 'Book' }
+        ]
+    }
+
+    changeHandler(event) {
+        var re = new RegExp('([A-Za-z]+)-?\\d*');
+        var source = event.target.id.match(re);
+        switch (source[1]) {
+            default:
+                this.nameSearch = source[1];
+                break;
+        }
+        return refreshApex(this.recordsUpdate);
     }
 }
