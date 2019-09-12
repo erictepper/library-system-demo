0113ef79d3be5d581f2dbdc066199872e61c6015
erictepper <eric.tepper@me.com>
2019-09-12 15:26:43 -0700

    Added method to get user records from wire.

--- force-app/main/default/lwc/borrowingRecords/borrowingRecords.js
+++ force-app/main/default/lwc/borrowingRecords/borrowingRecords.js
@@ -1,3 +1,27 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 
-export default class BorrowingHistory extends LightningElement {}
+import getBorrowingUsers from '@salesforce/apex/controller.getBorrowingUsers';
+
+export default class BorrowingHistory extends LightningElement {
+    @track checkoutUpdate
+    @track borrowingUsers;
+
+    @wire(getBorrowingUsers, {})
+    wiredLibraryItems(checkoutUpdate) {
+        this.checkoutUpdate = checkoutUpdate;
+        const { error, data } = checkoutUpdate;
+        if (data) {
+            // if there is no data, set this.borrowingUsers as undefined so the 
+            // html template does not render. 
+            if (data.length !== 0) {
+                this.borrowingUsers = data;
+            } else {
+                this.borrowingUsers = undefined;
+            }
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.borrowingUsers = undefined;
+        }
+    }
+}
