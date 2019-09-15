f51463ec64bfa20f940967233deaf7c8f8916175
erictepper <eric.tepper@me.com>
2019-09-14 21:44:29 -0700

    Organized code and added comments.

--- force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
+++ force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
@@ -7,18 +7,25 @@ import getLibraryItems from '@salesforce/apex/CatalogController.getLibraryItems'
 import getTotalPages from '@salesforce/apex/CatalogController.getTotalPages';
 
 export default class LibraryCatalog extends LightningElement {
+    // pageref for inter-component event handling
     @wire(CurrentPageReference) pageRef;
 
+    // Variables for refreshApex(this.{variable})
     @track libraryUpdate;
     @track libraryItems;
     @track totalPagesWire;
+
+    // search field variables
     @track barcodeSearch = "";
     @track typeSearch = "";
     @track nameSearch = "";
     @track statusSearch = "";
+
+    // paginator variables
     @track currPage = 1;
     @track totalPages = '';
 
+    // methods for inter-component event handling
     connectedCallback() {
         // subscribe to checkout event
         registerListener('checkout', this.checkoutHandler, this);
@@ -34,26 +41,24 @@ export default class LibraryCatalog extends LightningElement {
         refreshApex(this.libraryUpdate)
     }
 
-    @wire(getTotalPages, { 
-        barcodeSearch: '$barcodeSearch',
-        typeSearch: '$typeSearch',
-        nameSearch: '$nameSearch',
-        statusSearch: '$statusSearch'
-    })
-    wiredTotalPages(totalPagesWire) {
-        this.totalPagesWire = totalPagesWire;
-        const { error, data } = totalPagesWire;
-        if (data) {
-            this.totalPages = data.toString();
-            this.error = undefined;
-        } else if (error) {
-            this.error = error;
-            this.totalPages = '1';
-        } else {
-            this.totalPages = '1';
-        }
+    // gets options for the picklists in the search field
+    get typeOptions() {
+        return [
+            { label: 'All', value: '' },
+            { label: 'AV', value: 'AV Equipment' },
+            { label: 'Book', value: 'Book' }
+        ]
     }
 
+    get availabilityOptions() {
+        return [
+            { label: 'All', value: '' },
+            { label: 'Yes', value: 'true' },
+            { label: 'No', value: 'false' }
+        ]
+    }
+
+    // wire to get library items for display in the catalog
     @wire(getLibraryItems, { 
         barcodeSearch: '$barcodeSearch',
         typeSearch: '$typeSearch',
@@ -73,22 +78,7 @@ export default class LibraryCatalog extends LightningElement {
         }
     }
 
-    get typeOptions() {
-        return [
-            { label: 'All', value: '' },
-            { label: 'AV', value: 'AV Equipment' },
-            { label: 'Book', value: 'Book' }
-        ]
-    }
-
-    get availabilityOptions() {
-        return [
-            { label: 'All', value: '' },
-            { label: 'Yes', value: 'true' },
-            { label: 'No', value: 'false' }
-        ]
-    }
-
+    // handles changes from search fields/page number input
     changeHandler(event) {
         var digitMatcher;
         var re = new RegExp('([A-Za-z]+)-?\\d*');
@@ -137,7 +127,28 @@ export default class LibraryCatalog extends LightningElement {
         return refreshApex(this.libraryUpdate);
     }
 
-    // Resets page number when search terms are updated.
+    // wires the total number of pages for page display
+    @wire(getTotalPages, { 
+        barcodeSearch: '$barcodeSearch',
+        typeSearch: '$typeSearch',
+        nameSearch: '$nameSearch',
+        statusSearch: '$statusSearch'
+    })
+    wiredTotalPages(totalPagesWire) {
+        this.totalPagesWire = totalPagesWire;
+        const { error, data } = totalPagesWire;
+        if (data) {
+            this.totalPages = data.toString();
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.totalPages = '1';
+        } else {
+            this.totalPages = '1';
+        }
+    }
+
+    // Resets page number to 1 when search terms are updated.
     resetPageNumber() {
         var inputField = this.template.querySelector('.pnum');
         inputField.value = '1';
