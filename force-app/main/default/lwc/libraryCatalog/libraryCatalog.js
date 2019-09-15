6740e3522e43861d68338d7c91eb5a00852712a7
erictepper <eric.tepper@me.com>
2019-09-14 22:10:25 -0700

    libraryItems will now be set as an empty array if the wire returns an error.

--- force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
+++ force-app/main/default/lwc/libraryCatalog/libraryCatalog.js
@@ -73,7 +73,7 @@ export default class LibraryCatalog extends LightningElement {
             this.error = undefined;
         } else if (error) {
             this.error = error;
-            this.libraryItems = undefined;
+            this.libraryItems = [];
         }
     }
 
